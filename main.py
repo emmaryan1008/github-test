63f78a35d3bfcb33fde4cc900cc7ee41d84395b5
emmaryan1008 <likelee99@gmail.com>
2025-01-29 14:15:33 +0900

    main -update button code

--- main.py
+++ main.py
@@ -19,6 +19,20 @@ class Calculator(QWidget):
         self.btn1 =QPushButton('Message',self)
         # 해당하는 버튼이 클릭이 됬을때 이벤트를 발생 (activeMessage 함수를 실행)
         self.btn1.clicked.connect(self.activeMessage)
+        # ----------1차 수정 추가 부분 --------------
+        
+        # 버튼의 위치를 수정
+        # 수직 레이아웃 위젯 생성
+        vbox = QVBoxLayout()
+        # 비어있는 공간을 생성
+        vbox.addStretch(1)
+        # 버튼을 추가
+        vbox.addWidget(self.btn1)
+        # 비어있는 공간을 생성
+        vbox.addStretch(1)
+        # 빈공간 - 버튼 - 빈공간 순으로 수직 배치된 레이아웃 생성
+        self.setLayout(vbox)
+        
         self.setWindowTitle("Calculator") #새로운 화면에 제목
         self.resize(256,256) #새로운 화면의 사이즈
         self.show() #윈도우 화면을 출력
